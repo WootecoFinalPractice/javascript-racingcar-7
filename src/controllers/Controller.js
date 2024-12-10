d48b50d27638b2f851be5ac5d4c573796c23ba59
Beomtae <95863810+Beomtae@users.noreply.github.com>
2024-12-10 15:46:16 +0900

    ✨ feat: 플레이어마다 자동차 객체 생성

--- src/controllers/Controller.js
+++ src/controllers/Controller.js
@@ -1,12 +1,19 @@
 import InputView from '../views/InputView.js';
 import OutputView from '../views/OutputView.js';
 import Parser from '../utils/Parser.js';
+import Car from '../models/Car.js';
 
 class Controller {
   static async run() {
     const cars = await InputView.carInput();
     const car = Parser.parsingCar(cars);
+    const player = [];
+    for (let i = 0; i < car.length; i += 1) {
+      player.push(new Car(car[i]));
+    }
+    console.log(player);
     const time = await InputView.timeInput();
+    // Car.race(time);
     OutputView.print('실행 결과\n');
   }
 }
