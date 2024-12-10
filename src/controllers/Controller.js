d18705d8e51a5020ae285708ae428a0edaf8cae3
Beomtae <95863810+Beomtae@users.noreply.github.com>
2024-12-10 15:37:06 +0900

    ✨ feat: 자동차 여러대 입력 받은 후 분리

--- src/controllers/Controller.js
+++ src/controllers/Controller.js
@@ -1,9 +1,11 @@
 import InputView from '../views/InputView.js';
 import OutputView from '../views/OutputView.js';
+import Parser from '../utils/Parser.js';
 
 class Controller {
   static async run() {
-    const car = await InputView.carInput();
+    const cars = await InputView.carInput();
+    const car = Parser.parsingCar(cars);
     const time = await InputView.timeInput();
     OutputView.print('실행 결과\n');
   }
---
+++ src/utils/Parser.js
@@ -0,0 +1,7 @@
+class Parser {
+  static parsingCar(cars) {
+    const car = cars.split(',');
+    return car;
+  }
+}
+export default Parser;
